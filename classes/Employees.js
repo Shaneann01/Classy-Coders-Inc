c79483c48bbc0db0c06e704a124bb9039447eb28
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-10-25 17:20:40 +0100

    done all except the getTotalParoll

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,45 @@
 class Employees {
+  #salary;
+  #isHired;
+  static allEmployees = [];
 
+  static getEmployees() {
+    return this.allEmployees;
+  }
 
+  static getTotalPayroll() {
+    let total = 0;
+    total = this.Employees["#salary"];
+    return total;
+  }
 
-    
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.#salary = salary;
+    this.#isHired = true;
+    Employees.allEmployees.push(this);
+  }
+  getSalary() {
+    return this.#salary;
+  }
+
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+
+  getStatus() {
+    return this.#isHired;
+  }
+
+  setStatus(command) {
+    command = command.toLowerCase();
+    if (command !== "hire") {
+      this.#isHired = false;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
